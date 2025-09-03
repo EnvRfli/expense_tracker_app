569f999eb5573cdd85d17c20d58ff307e4284ec6
RafliEnv <rafliagusta111@gmail.com>
2025-09-03 14:43:20 +0700

    refactor: remove redundant comments in AppLockState to enhance code clarity

--- lib/services/app_lock_state.dart
+++ lib/services/app_lock_state.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/foundation.dart';
 
-/// Global app state to track various states that should pause idle detection
 class AppLockState {
   static final ValueNotifier<bool> isLockVisible = ValueNotifier<bool>(false);
   static final ValueNotifier<bool> isSplashVisible = ValueNotifier<bool>(false);
 
-  /// Returns true if idle detection should be active
   static bool get shouldDetectIdle {
     return !isLockVisible.value && !isSplashVisible.value;
   }
 
-  /// Set lock screen visibility
   static void setLockVisible(bool visible) {
     isLockVisible.value = visible;
   }
 
-  /// Set splash screen visibility
   static void setSplashVisible(bool visible) {
     isSplashVisible.value = visible;
   }
